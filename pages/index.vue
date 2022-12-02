489ba6fbfc806e4eb9ef31c92d6776a295d08089
Pauline JOBERT <71442910+PaulineJobs@users.noreply.github.com>
2022-12-02 05:40:28 +0100

    link home to game

--- pages/index.vue
+++ pages/index.vue
@@ -25,26 +25,35 @@
         id="card"
         class="m-14 flex place-items-center justify-between rounded-2xl border border-gray-800 bg-white/10 shadow-xl backdrop-blur-sm hover:shadow-2xl"
       >
-        <div class="block">
+        <div class="block justify-center">
           <div class="px-6 text-center text-2xl text-gray-800">
             Vous incarnerez un jeune adolescant face à une situation de la vie
-            courante dont vous devez dicterer pour créer son aventure. À vous de
-            jouer !
+            courante dont vous devez dicter les choix pour créer son aventure. À
+            vous de jouer !
           </div>
-          <div class="place-items-center">
-            <button
-              class="group relative mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800"
-            >
-              <span
-                class="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900"
+
+          <!-- <div class="ite">
+            <a href="/game">
+              <button
+                class="mt-4 mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800"
               >
-                Purple to blue
-              </span>
-            </button>
+                Play
+              </button>
+            </a>
+          </div> -->
+
+          <div class="flex justify-center">
+            <a href="/game">
+              <button
+                class="my-2 rounded bg-purple-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+              >
+                Play
+              </button>
+            </a>
           </div>
         </div>
         <div class="p-10">
-          <img src="@/assets/img/home/logo.png" width="512" />
+          <img src="@/assets/img/home/play.png" width="512" />
         </div>
       </div>
     </div>
