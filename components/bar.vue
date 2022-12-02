2d9fc60165cf08564a50d4b8beaa4559bbc8a9ee
Arthuino <vigouroux.art@gmail.com>
2022-12-02 07:28:20 +0100

    sauver les apparences...

--- components/bar.vue
+++ components/bar.vue
@@ -6,16 +6,20 @@
       class="border-t-10 m-4 rounded border-primary bg-transparent p-4 text-white"
     >
       <!-- {{ scene.narrativeText }} -->
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus excepturi
-      hic ullam dolores ratione, laudantium sed consequuntur doloremque sequi
-      unde saepe deserunt maiores tempore aut culpa assumenda exercitationem.
-      Animi, necessitatibus?
+      Cette soirée s'est jusqu'ici passée à merveille. Tu t'es bien amusé avec
+      tes amis, et il n'est pas encore trop tard. Tu a rencontré une fille
+      irresistible et très sympa en début de soirée, Sandra, avec laquelle tu as
+      partagé quelques verres. Ca tombe bien, tu semble l'interrésser aussi...
+      Alors que tu t'appretais à recommander pour vous deux, elle te propose de
+      plutot prendre ce dernier verre chez elle.
     </div>
 
     <div class="m-4 grid grid-flow-col content-center gap-5">
-      <Button>Carotte</Button>
-      <Button>Patate</Button>
-      <Button>Aubergine</Button>
+      <Button>La suivre dehors</Button>
+      <Button>Ne sois pas bête, bien sûr que tu la suis !</Button>
+      <Button
+        >Ca parait être une bonne idée d'accepter sa proposition...</Button
+      >
     </div>
   </div>
 </template>
